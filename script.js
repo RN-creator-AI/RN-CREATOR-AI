5b11a8aa1bc97ec1c1b60a03ff0588400b9c7e6a
RN-creator-AI <rkriti344@gmail.com>
2026-07-16 12:13:19 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// RN Creator AI Script
+
+function showMessage(message){
+    alert(message);
+}
+
+function openChat(){
+    window.location.href="chat.html";
+}
+
+function openImage(){
+    window.location.href="image.html";
+}
+
+function openVideo(){
+    window.location.href="video.html";
+}
+
+function openSEO(){
+    window.location.href="seo.html";
+}
+
+function openLogin(){
+    window.location.href="login.html";
+}
+
+console.log("🚀 RN Creator AI Loaded Successfully"); 
+<script src="script.js"></script>
